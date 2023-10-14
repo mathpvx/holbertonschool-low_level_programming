69198aa51240f9c78865edce95e737c9f21548d1
mathpvx <mathilde.pavaux@gmail.com>
2023-10-14 09:21:41 -0700

    size

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -8,7 +8,8 @@
  *
  * Return: 0 (success)
  */
-int main(void) {
+int main(void) 
+{
 printf("Size of a char: %ld byte(s)\n", sizeof(char));
 printf("Size of an int: %ld byte(s)\n", sizeof(int));
 printf("Size of a long int: %ld byte(s)\n", sizeof(long int));
