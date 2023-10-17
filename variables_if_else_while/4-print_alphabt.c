b12277422d1efc5d1e33a132121b8e2baed89c26
mathpvx <mathilde.pavaux@gmail.com>
2023-10-17 12:27:52 -0700

    4

--- variables_if_else_while/4-print_alphabt.c
+++ variables_if_else_while/4-print_alphabt.c
@@ -10,11 +10,11 @@ int main(void)
 	char a;
 
 	for (a = 'a'; a <= 'z'; a++)
-		
-		if (a != 'q' && a != 'e')
+
+	if (a != 'q' && a != 'e')
 	{
 	putchar(a);
-        }
+	}
 	putchar('\n');
 
 	return (0);
