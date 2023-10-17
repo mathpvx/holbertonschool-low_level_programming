8d371198c2d4dc1d6aaa9d20306845d0f56e75f8
mathpvx <mathilde.pavaux@gmail.com>
2023-10-17 11:53:58 -0700

    3

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ *
+ */
+int main(void)
+{
+	char l;
+	for (l = 'a'; l <= 'z'; l++)
+	putchar('l');
+
+	char U;
+	for (U = 'A'; U <= 'Z'; U++)
+	putchar('\n');
+
+	return (0);
+}
