6e6935e4688e5170574bdd974ffa996b19d471ab
mathpvx <mathilde.pavaux@gmail.com>
2023-10-17 11:22:00 -0700

    1

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -21,15 +21,15 @@ int main(void)
 
 	if (last_digit > 5)
 	{
-	printf(" and is greater than 5 ");
+	printf(" and is greater than 5\n");
 	}
 	else if (last_digit == 0)
 	{
-	printf(" and is 0 ");
+	printf(" and is 0\n");
 	}
 	else
 	{
-	printf(" and is less than 6 and not 0 ");
+	printf(" and is less than 6 and not 0\n");
 	}
 
 	return (0);
