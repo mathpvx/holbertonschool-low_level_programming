e98991c6549cdf0ec12ec573a9afe29dd01191e1
mathpvx <mathilde.pavaux@gmail.com>
2023-10-17 11:57:43 -0700

    3

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,14 +1,18 @@
 #include <stdio.h>
 /**
+ * main - prints alphabet in lowercase and uppercase
  *
+ * Result: always 0 (success)
  */
 int main(void)
 {
 	char l;
+
 	for (l = 'a'; l <= 'z'; l++)
 	putchar('l');
 
 	char U;
+
 	for (U = 'A'; U <= 'Z'; U++)
 	putchar('\n');
 
