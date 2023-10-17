56d991695cf30d54c9321133490694c7629d579c
mathpvx <mathilde.pavaux@gmail.com>
2023-10-17 14:18:04 -0700

    6

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints base ten
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{	
+	int bt;
+
+	for (bt = 0; bt < 10; bt++)
+	{
+	putchar(bt);
+	}
+	putchar('\n');
+
+	return (0);
+}
