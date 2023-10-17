d1b8e490b06437786794014095d3118f0a1347e3
mathpvx <mathilde.pavaux@gmail.com>
2023-10-17 14:05:55 -0700

    5

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints base 10 numbers
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	int bt;
+	for (bt = 0, bt < 10, bt++)
+	{
+	printf("%d", bt);
+	}
+	putchar('\n');
+
+	return (0);
+}
