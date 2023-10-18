99946ef874a70f2824a5aea2d12d1499f981f016
mathpvx <mathilde.pavaux@gmail.com>
2023-10-18 06:01:14 -0700

    8

--- variables_if_else_while/8-print_base16.c
+++ variables_if_else_while/8-print_base16.c
@@ -2,20 +2,26 @@
 
 /**
  * main - prints numbers of base 16
+ *
+ * Return: always 0 (success)
  */
 
 int main(void)
 {
 	int n;
-	for (n = '0'; n <= '9'; n++)
+	char l;
+
+	for (n = 0; n <= 9; n++)
 	{
 	putchar(n);
 	}
 
-	char l;
 	for (l = 'a'; l <= 'f'; l++)
 	{
 	putchar(l);
 	}
+
+	putchar('\n');
+
 	return (0);
 }
