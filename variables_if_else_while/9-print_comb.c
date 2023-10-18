4c62d1804def2b09295dfdc91e8e2a6828154d07
mathpvx <mathilde.pavaux@gmail.com>
2023-10-18 06:42:23 -0700

    9

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9 with comma
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char d;
+
+	for (d = '0'; d <= '9'; d++)
+	{
+	putchar(' ');
+	putchar(d);
+	putchar(',');
+	}
+	putchar('\n');
+
+	return (0);
+}
