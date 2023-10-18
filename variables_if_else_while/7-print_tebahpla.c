e0a507b09cc12eac5fed8e2ac3e590692f9ae461
mathpvx <mathilde.pavaux@gmail.com>
2023-10-18 00:37:50 -0700

    7

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - prints alphabet in reverse
+ *
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	char herve
+
+	for(herve = 'z'; herve <= 'a'; herve++)
+	{
+	putchar(herve)
+	}
+	putchar('\n')
+
+	return (0);
+}
