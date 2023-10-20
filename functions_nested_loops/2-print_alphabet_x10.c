28056751c52238d3671ff35d5ff582e5e8d718dd
mathpvx <mathilde.pavaux@gmail.com>
2023-10-20 01:56:38 -0700

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -1,11 +1,10 @@
 #include "main.h"
 
 /**
- *main - void print_alphabet_x10(void) print alphabet 10 times
+ * print_alphabet_x10 - print alphabet 10 times
  *
  * Return: always 0 (success)
  */
-
 void print_alphabet_x10(void)
 {
 	char ch;
