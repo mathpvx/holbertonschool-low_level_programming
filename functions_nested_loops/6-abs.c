d784d41213c4419124ba1a42b5022e5f7a054a7e
mathpvx <mathilde.pavaux@gmail.com>
2023-10-20 03:33:59 -0700

    6

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @n: number to compute
+ *
+ * Return: n if number is positive, -n if number is negative
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
