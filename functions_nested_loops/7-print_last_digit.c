008211ef7bfba6edbf617317979631df42937896
mathpvx <mathilde.pavaux@gmail.com>
2023-10-20 03:56:44 -0700

    7

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * print_last_digit - makes sense itself
+ *
+ * @n: number
+ *
+ * @a: last digit
+ *
+ * Return: a
+ */
+
+int print_last_digit(int n)
+{
+	int a;
+
+	if (n < 0)
+	{
+		n = -n;
+	}
+
+	a = n % 10;
+		if (a < 0)
+		{
+			a = -a;
+		}
+	_putchar(a);
+
+	return (a);
+}
+
