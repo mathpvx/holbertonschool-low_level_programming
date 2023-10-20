633db4323c6c00c34f9f88756bff2f64abf28e09
mathpvx <mathilde.pavaux@gmail.com>
2023-10-20 04:58:47 -0700

    8

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ *
+ */
+
+void jack_bauer(void)
+{
+	int a;
+	int b;
+	int c;
+	int d;
+	int e;
+	
+	for (a = 0; a <= 2; a++)
+	{
+		_putchar(a);
+	}
+
+	 for (b = 0; b <= 2; b++)
+        {
+                _putchar(b);
+        }
+	  for (c = 0; c <= 2; c++)
+        {
+                _putchar(c);
+        }
+	   for (d = 0; d <= 2; d++)
+        {
+                _putchar(d);
+        }
+	    for (e = 0; e <= 2; e++)
+        {
+                _putchar(e);
+        }
+	    _putchar('\n');
+}
