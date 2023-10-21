cb28cffcb54b944bd37a6f17f9816398aea25c68
mathpvx <mathilde.pavaux@gmail.com>
2023-10-21 06:50:06 -0700

    4

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul - multiply 2 int
+ * @a: int multiply to b
+ * @b: int multiply to a
+ * Return: the result of the operation
+ */
+int mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+
+	return (c);
+}
