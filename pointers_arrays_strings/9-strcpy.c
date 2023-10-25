b64e6ee8dc4b9bdd41d17512319660e3cd49b860
mathpvx <mathilde.pavaux@gmail.com>
2023-10-25 00:54:20 -0700

    9

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copies and paste a string
+ *
+ * @*dest: point the buffer the string is going to be paste to
+ *
+ * @*src: the string that is copied
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int len, i;
+
+	len = 0;
+		while (src[len] != '\0')
+		{
+			len++;
+		}
+		for (i = 0; i < len; i++)
+		{
+			dest[i] = src[i];
+		}
+		dest[i] = '\0';
+		
+		return (dest);
+}
