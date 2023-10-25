669391ccd10367b6ad98fd0075d494e8e5253a7e
mathpvx <mathilde.pavaux@gmail.com>
2023-10-25 06:12:30 -0700

    5

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,24 +8,23 @@
 
 void rev_string(char *s)
 {
-	int a, len;
+	char tmp;
+	int i, len, len1;
 
-	char *begin, *end = s;
+	len = 0;
+	len1 = 0;
 
-	for (a = 0; s[a] != '\0' && s[a + 1] != '\0'; a++)
+	while (s[len] != '\0')
 	{
-		end++;
+		len++;
 	}
-	len = a + 1;
-	begin = s;
-	for (a = 0; a < len / 2; a++)
+
+	len1 = len - 1;
+
+	for (i = 0; i < len / 2; i++)
 	{
-		char x;
-		x = *end;
-		*end = *begin;
-		*begin = x;
-		begin++;
-		end--;
+		tmp = s[i];
+		s[i] = s[len1];
+		s[len1--] = tmp;
 	}
-	end[len + 1] = '\0';
 }
