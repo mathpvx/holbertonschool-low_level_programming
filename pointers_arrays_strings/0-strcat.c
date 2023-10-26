dcd740a3da6c4b90132e47e8f28cf5ba43f0402d
mathpvx <mathilde.pavaux@gmail.com>
2023-10-26 07:39:15 -0700

    0

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _strcat - places the src string after the dest string
+ *
+ *  @dest: first string
+ *
+ *  @src: second string
+ */
+
+char *_strcat(char *dest, char *src)
+{
+	int i = 0;
+	int j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] = src[j];
+		j++;
+		i++;
+	}
+	
+	dest[i] = '\0';
+	return (dest);
+}
