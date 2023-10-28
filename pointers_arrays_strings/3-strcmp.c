d1828e03b6c5d869d5f3fbeb67c999dac6329ec9
mathpvx <mathilde.pavaux@gmail.com>
2023-10-28 02:10:35 -0700

    3

---
+++ pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _strcmp - compares two strings
+ *
+ * @s1: first string
+ *
+ * @s2: second string
+ *
+ * Return: 0 if equal, positive if s1 greater than s2, negative if the opposite. 
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 == *s2)
+	{
+		if (*s1 == '\0')
+		{
+			return (0);
+		}
+		s1++;
+		s2++;
+	}
+	return (*s1 - *s2);
+}
