fb53eeba3f455c45c9eeb35128ae3f893d3e003c
mathpvx <mathilde.pavaux@gmail.com>
2023-10-28 02:45:02 -0700

    5

---
+++ pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * string_toupper - convert to uppercase
+ *
+ * @s: string
+ *
+ * Return: s
+ */
+
+char *string_toupper(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] >= 'a' && s[i] <= 'z')
+			s[i] = s[i] - 32;
+	}
+	return (s);
+}
