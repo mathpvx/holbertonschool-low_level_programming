7f92bd2f0904e34eb2ebaf413903dbddf80c2bf1
mathpvx <mathilde.pavaux@gmail.com>
2023-10-28 03:06:30 -0700

    7

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * leet - converts in leet speak
+ *
+ * @s: string
+ *
+ * Return: s
+ */
+
+char *leet(char *s)
+{
+	int i, j;
+
+	char *a = "aAeEoOtTlL";
+	char *b = "4433007711";
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			if (s[i] == a[j])
+			{
+				s[i] = b[j];
+			}
+		}
+	}
+
+	return (s);
+}
