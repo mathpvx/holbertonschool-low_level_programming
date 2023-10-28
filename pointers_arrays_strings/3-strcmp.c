55d698ffb392cef75b3ce974341464a837d621af
mathpvx <mathilde.pavaux@gmail.com>
2023-10-28 02:12:22 -0700

    3

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -7,7 +7,7 @@
  *
  * @s2: second string
  *
- * Return: 0 if equal, positive if s1 greater than s2, negative if the opposite. 
+ * Return: 0 if equal, positive if s1 greater than s2, negative opposite.
  */
 
 int _strcmp(char *s1, char *s2)
