de7540d2183a89baf35433f28380f419489cd7af
mathpvx <mathilde.pavaux@gmail.com>
2023-10-30 08:54:52 -0700

    3

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ *
+ * @n: given number
+ *
+ * Return: -1 if negative
+ */
+
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+	return (n * factorial(n - 1));
+}
