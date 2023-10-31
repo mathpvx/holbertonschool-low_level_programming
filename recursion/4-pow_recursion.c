bc1218adcd1bdaade21aa8804eb2dc46043260d7
mathpvx <mathilde.pavaux@gmail.com>
2023-10-31 03:20:12 -0700

    4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - returns the value of x raised to the power of y
+ *
+ * @x: value multiplied by itself
+ *
+ * @y: the number of times x is multiplied
+ *
+ * Return: the result
+ */
+
+int _pow_recursion(int x, int y)
+{
+
+	if (y < 0)
+	{
+		return (-1);
+	}
+	if (y == 0)
+	{
+		return (1);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+
+}
