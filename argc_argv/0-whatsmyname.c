2e0b6e2ab656a55856e124505bfa3106cf0c0b05
mathpvx <mathilde.pavaux@gmail.com>
2023-11-02 03:24:53 -0700

    0

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - prints the name of its own
+ *
+ * @argc: counts the number of arguments
+ *
+ * @argv: array of strings of command line arguments 
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", *argv);
+	return (0);
+}
