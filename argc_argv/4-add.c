c49b12e7f9987ddb0c9c3a884391cf7634245c1a
mathpvx <mathilde.pavaux@gmail.com>
2023-11-03 07:43:09 -0700

    4

---
+++ argc_argv/4-add.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * main - adds positive number
+ *
+ * @argc: couts number of arguments
+ *
+ * @argv: arguments
+ *
+ * Return: 1 or 0
+ */
+
+int main(int argc, char *argv[])
+{
+	int sum, num, i, j, k;
+
+	sum = 0;
+
+	for (i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (argv[i][j] > '9' || argv[i][j] < '0')
+			{
+				puts("Error");
+				return (1);
+			}
+		}
+	}
+
+	for (k = 1; k < argc; k++)
+	{
+		num = atoi(argv[k]);
+		if (num >= 0)
+		{
+			sum += num;
+		}
+	}
+
+	printf("%d\n", sum);
+	return (0);
+}
