68bffc2432f55d882806d012a65670c3e2d25fb4
mathpvx <mathilde.pavaux@gmail.com>
2023-11-13 07:41:15 -0800

    0

---
+++ structures_typedef/README.md
@@ -0,0 +1,1 @@
+repo about structures
---
+++ structures_typedef/dog.h
@@ -0,0 +1,16 @@
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - dog info
+ * @name : name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+
+	char *name;
+	float age;
+	char *owner;
+};
+#endif
