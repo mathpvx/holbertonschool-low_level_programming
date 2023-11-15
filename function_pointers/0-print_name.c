1180368037680fcd86b764abd6b77f761d49b91b
mathpvx <mathilde.pavaux@gmail.com>
2023-11-15 00:44:11 -0800

    0

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,12 +1,14 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include "function_pointers.h"
+
 /**
- * print_name - function that takes another function as an argument
- * @name: the name
- * @f: pointer to a function that actually prints a name
+ * print_name - prints a name
+ * @name: name to print
+ * @f: pointer to the printing function
  */
 void print_name(char *name, void (*f)(char *))
 {
+	if (!name || !f)
+		return;
 	f(name);
 }
