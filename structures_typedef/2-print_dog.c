dad7385d9917c9c3f13fbf50ff0f7f5da8fd3a25
mathpvx <mathilde.pavaux@gmail.com>
2023-11-15 01:17:46 -0800

    2

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -9,6 +9,14 @@
 
 void print_dog(struct dog *d)
 {
+	if (d == NULL)
+		return;
+
+	if (d->name == NULL)
+		d->name = "(nil)";
+	if (d->owner == NULL)
+		d->owner = "(nil)";
+
 	printf("Name: %s\n", d->name);
 	printf("Age: %.6f\n", d->age);
 	printf("Owner: %s\n", d->owner);
