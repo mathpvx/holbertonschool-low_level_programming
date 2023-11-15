30b50a987068dc7541c0c494a993f0f9f9e26d3b
mathpvx <mathilde.pavaux@gmail.com>
2023-11-15 01:14:38 -0800

    header

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -13,4 +13,14 @@ struct dog
 	float age;
 	char *owner;
 };
+
+/**
+ * dog_t - typedef for struct dog
+ */
+typedef struct dog dog_t;
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
 #endif
