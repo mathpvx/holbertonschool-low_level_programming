1f71b333960e835c4594fa8457cfdd85269e9137
mathpvx <mathilde.pavaux@gmail.com>
2023-11-15 01:06:03 -0800

    1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -11,7 +11,7 @@
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-		d = malloc(sizeof(struct dog));
+		return;
 	d->name = name;
 	d->age = age;
 	d->owner = owner;
