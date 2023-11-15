629f57856a819052e990c811cf4a9a2124356fe5
mathpvx <mathilde.pavaux@gmail.com>
2023-11-15 01:46:15 -0800

    1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "function_pointers.h"
+/**
+ * array_iterator - exectues a function given as a parameter
+ * @array: array
+ * @size: size of the array
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	unsigned int i;
+
+	for (i = 0; i < size; i++)
+		action(array[i]);
+}
