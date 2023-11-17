86d2c3c0d71e83b249cde6dc617ba9a62a069995
mathpvx <mathilde.pavaux@gmail.com>
2023-11-17 02:27:55 -0800

    0

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+/**
+ * sum_them_all - sums all the arguments
+ *
+ * @n: number of arguments
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int sum;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start (args, n);
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, unsigned int);
+	}
+	va_end(args);
+
+	return (sum);
+}
