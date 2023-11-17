c7fa82194eacb5aa6a0a0ca468777d38ee09e325
mathpvx <mathilde.pavaux@gmail.com>
2023-11-17 05:40:37 -0800

    header

--- variadic_functions/variadic_functions.h
+++ variadic_functions/variadic_functions.h
@@ -1,6 +1,8 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
+void print_strings(const char *separator, const unsigned int n, ...);
 #endif
