33cf259a0b0a423591dc847d3c7696796ed88e22
mathpvx <mathilde.pavaux@gmail.com>
2023-11-17 05:40:07 -0800

    2

---
+++ variadic_functions/2-print_strings.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include <stdio.h>
+#include "variadic_functions.h"
+/**
+ * print_strings - prints strings
+ * @separator: between each string
+ * @n: number of strings passed as arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	char * value;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		value = va_arg(args, char*);
+		printf("%s", value);
+		if ( i < n - 1 && separator != NULL)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(args);
+}
