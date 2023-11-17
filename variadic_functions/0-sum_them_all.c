53aef6243e056d84a6c159d5db64be45ffb38b61
mathpvx <mathilde.pavaux@gmail.com>
2023-11-17 02:31:09 -0800

    0

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -4,6 +4,7 @@
  * sum_them_all - sums all the arguments
  *
  * @n: number of arguments
+ * Return: 0 if n = 0
  */
 int sum_them_all(const unsigned int n, ...)
 {
@@ -15,7 +16,7 @@ int sum_them_all(const unsigned int n, ...)
 	{
 		return (0);
 	}
-	va_start (args, n);
+	va_start(args, n);
 	sum = 0;
 	for (i = 0; i < n; i++)
 	{
