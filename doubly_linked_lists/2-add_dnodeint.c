070e1e6f4ddb2d7c94488b0e0a7305b9a2ec68b5
mathpvx <mathilde.pavaux@gmail.com>
2023-11-29 02:09:20 -0800

    2

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -9,6 +9,7 @@
 dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new;
+	dlistint_t *h;
 
 	new = malloc(sizeof(dlistint_t));
 	if (new == NULL)
@@ -16,11 +17,20 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	new->n = n;
 	new->prev = NULL;
-	new->next = *head;
+	h = *head;
 
-	if (*head != NULL)
-		(*head)->prev = new;
+	if (h != NULL)
+	{
+		while (h->prev != NULL)
+			h = h->prev;
+	}
+
+	new->next = h;
+
+	if (h != NULL)
+		h->prev = new;
 
 	*head = new;
+
 	return (new);
 }
