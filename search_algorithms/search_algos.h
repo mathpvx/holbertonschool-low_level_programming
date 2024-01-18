ac3427b19b34db9aac0c077b51abddbe20baaded
mathpvx <mathilde.pavaux@gmail.com>
2024-01-18 01:34:09 -0800

    add librairies

--- search_algorithms/search_algos.h
+++ search_algorithms/search_algos.h
@@ -1,6 +1,8 @@
 #ifndef SEARCH_ALGOS_H
 #define SEARCH_ALGOS_H
-
+/**libraries*/
+#include <stddef.h>
+#include <stdio.h>
 /**prototypes*/
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
